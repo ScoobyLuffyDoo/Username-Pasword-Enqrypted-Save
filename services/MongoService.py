877ca2ddb956b1c210aba59781c9b088d1948b15
Johannes Smal <johanness@tihsa.co.za>
2021-05-07 10:59:36 +0200

    Local Connection TO mongodb

--- services/MongoService.py
+++ services/MongoService.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+import pymongo 
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
@@ -7,11 +7,15 @@ def loadkey():
     return open("Mongo.key","r").read()
 
 
+myclient = pymongo.MongoClient(loadkey())
+
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+  print("The database exists.")
+else:
+    print("DB does not exist")
+# # Issue the serverStatus command and print the results
+# serverStatusResult=db.command("serverStatus")
+# print(serverStatusResult)
 
 
-client = MongoClient()
-db = client.test
-
-# Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-print(serverStatusResult)
